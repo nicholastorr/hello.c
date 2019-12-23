8b9ed639e5f69a9b788181e88453bbaa6db99520
nicholastorr <52306328+nicholastorr@users.noreply.github.com>
2019-12-23 15:27:54 -0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// Asks for your name then prints it out
+int main(void)
+{
+    string name = get_string("What is your name?\n");
+    printf("hello, name\n");
+}
